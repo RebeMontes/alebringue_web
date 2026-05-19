bef56ecea77ad86475290d05c02ec59c62c8583f
Alexis Cespedes <l22260112@matamoros.tecnm.mx>
2026-05-19 02:43:19 -0500

    css-completos

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -9,7 +9,7 @@
     <!-- TailwindCSS v3 CDN -->
     <!-- <script src="https://cdn.tailwindcss.com"></script> -->
 </head>
-<body class="welcome-page bg-gradient-purple min-h-screen">
+<body class="welcome-page min-h-screen">
 
     <!-- Contenedor principal -->
     <div class="min-h-screen flex flex-col">
